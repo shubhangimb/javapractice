93bb55e556e90ee7a1cdcef20cec62d6ae329718
shubhangimb <bhaisare.shubha@gmail.com>
2023-07-12 23:28:17 +0530

    First GIT commit for java

---
+++ src/javaexmp/hi.java
@@ -0,0 +1,12 @@
+package javaexmp;
+
+public class hi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("hi");
+    System.out.println("done");
+    
+	}
+
+}
